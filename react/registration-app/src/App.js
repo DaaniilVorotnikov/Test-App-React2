61e579b827797427389e16343cba31d5e2aa2b02
DaaniilVorotnikov <daavorotnikov@gmail.com>
2021-07-22 17:43:58 +0300

    commit react

--- react/registration-app/src/App.js
+++ react/registration-app/src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useForm} from "react-hook-form";
 
 function App() {
+
+  const{
+    register,
+    handleSubmit,
+    formState: {errors, submitCount},
+    watch
+  }=useForm({mode:'onChange'});
+
+  const onSubmit = data => console.log('Отправлено', data);
+  const name = watch('name');
+
+  console.log('Имя:' + name);
+  console.log(errors);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit = {handleSubmit(onSubmit)}>
+          <h2 style={{color:"white", backgroundColor: "silver"}}>Registration form</h2>
+          <input
+            {...register('name', {required: true, maxLength: 15})}
+          />
+          {errors.name && <i>Обязательное поле не более 15 символов!!!</i>}
+          <hr/>
+          <input
+            {...register('password')} 
+          />
+          <hr/>
+          <input type= "submit" />
+       {submitCount}
+        </form> 
       </header>
     </div>
   );
