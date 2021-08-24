2e667fa749aa35bfc7e96e166f6d56571ab11700
DaaniilVorotnikov <daavorotnikov@gmail.com>
2021-08-24 17:39:29 +0300

    got-app-context

---
+++ react/redux-app/src/components/counter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import{connect} from 'react-redux';
+import * as actions from '../actions';
+
+
+const Counter = ({counter, inc, dec, rnd}) => {
+    return(
+        <div className="jumbotron">
+        <h1>{counter}</h1>
+        <button onClick={dec} className="btn btn-primary">Dec</button>
+        <button onClick={inc} className="btn btn-primary">Inc</button>
+        <button onClick={rnd} className="btn btn-primary">RND</button>
+      </div>
+    );
+}
+const mapStateToProps = (state) => {
+  return{
+      counter: state
+  }
+}
+
+export default connect(mapStateToProps, actions)(Counter);
